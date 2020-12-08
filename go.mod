242b1150284a942451f9d493daad2662caf26057
Baodong <wwwicbd@gmail.com>
2020-12-08 14:42:46 +0800

    feat: Add autotls

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.15
 
 require (
 	github.com/gin-contrib/cors v1.3.1
+	github.com/gin-gonic/autotls v0.0.3
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-playground/validator/v10 v10.2.0
 	github.com/icbd/gorm-migration v0.0.3
--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"github.com/gin-gonic/autotls"
 	"github.com/icbd/gohighlights/config"
 	"github.com/icbd/gohighlights/indices"
 	"github.com/icbd/gohighlights/models"
 	"github.com/icbd/gohighlights/models/migrations"
 	"github.com/icbd/gohighlights/routes"
 	mgr "github.com/icbd/gorm-migration"
+	"github.com/spf13/cast"
 	"log"
 	"net/http"
 )
@@ -24,7 +26,10 @@ func main() {
 	migrations.Migrate(mgr.Check)
 	indices.Ping()
 
-	if err := server.ListenAndServe(); err != nil {
-		log.Fatal(err)
+	domains := cast.ToStringSlice(config.Get("app.domains"))
+	if domains == nil {
+		log.Fatal(server.ListenAndServe())
+	} else {
+		log.Fatal(autotls.Run(server.Handler, domains...))
 	}
 }
